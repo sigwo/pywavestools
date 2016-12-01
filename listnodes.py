32eafa47bdc6fcf46acda65e80ac3bb90bc58fbb
pywaves <pblck@protonmail.com>
2016-12-01 11:25:50 +0100

    added breakdown by country

--- listnodes.py
+++ listnodes.py
@@ -4,8 +4,9 @@ from ipwhois import IPWhois
 
 node = 'nodes.wavesnodes.com'
 peers = requests.get('https://' + node + '/peers/connected').json()['peers']
-print("  #   Node name                     IP address    Country  Network              Network description")
+print("  #  Node name                      IP address    Country  Network              Network description")
 print("-" * 120)
+nodes_by_country = {}
 for i, peer in enumerate(peers):
     match = re.search('.*\/(.*):.*', peer['address'])
     if match:
@@ -23,7 +24,18 @@ for i, peer in enumerate(peers):
                 descr = res['description'].replace('\n', ' ')
         except:
             pass
-        print ("%3i  %-30s %-15s  %-2s    %-20s %-40s" % (i + 1, peer['peerName'][:30], ip, country[:2], net[:20], descr[:40]))
+        if country in nodes_by_country.keys():
+            nodes_by_country[country] += 1
+        else:
+            nodes_by_country[country] = 1
+        print ("%3d  %-30s %-15s  %-2s    %-20s %-40s" % (i + 1, peer['peerName'][:30], ip, country[:2], net[:20], descr[:40]))
+print("-" * 120)
+print
+print("Country  # of nodes")
+print("-" * 20)
+for c in sorted(nodes_by_country.items(), key=lambda x: -x[1]):
+    print ("  %-2s        %3d" % (c[0], c[1]))
+print
 print("-" * 120)
 
             
