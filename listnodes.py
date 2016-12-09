a94a5fe18056db85066d8f600f90c9e59e3c4c7e
pywaves <pblck@protonmail.com>
2016-12-09 07:29:38 +0100

    minor fixes and -a option to list known nodes

--- listnodes.py
+++ listnodes.py
@@ -1,6 +1,12 @@
 import requests
 import re
 from ipwhois import IPWhois
+import argparse
+
+parser = argparse.ArgumentParser(description='List nodes of the Waves network')
+parser.add_argument('-a', '--all', action='store_true', help='includes removed or temporary offline nodes')
+
+args = parser.parse_args()
 
 checked_nodes=[]
 
@@ -11,12 +17,13 @@ def getPeers(node):
         if node not in checked_nodes:
             checked_nodes.append(node)
             try:
-                tmp_peers = requests.get('https://' + node + '/peers/connected', timeout=0.5).json()['peers']
-                print("Getting peers from https://%s" % node)
+                tmp_peers = requests.get(node + '/peers/connected', timeout=0.5).json()['peers']
+                print("Getting peers from %s" % node)
             except:
+                pass
+            if args.all:
                 try:
-                    tmp_peers = requests.get('http://' + node + '/peers/connected', timeout=0.5).json()['peers']
-                    print("Getting peers from http://%s" % node)
+                    tmp_peers = tmp_peers + requests.get(node + '/peers/all', timeout=0.5).json()
                 except:
                     pass
             if len(tmp_peers)>0:
@@ -25,13 +32,13 @@ def getPeers(node):
                     match = re.search('.*\/(.*):.*', p['address'])
                     if match:
                         ip = match.group(1)
-                        tmp_peers = getPeers(ip + ':6869')
+                        tmp_peers = getPeers('http://' + ip + ':6869')
                         peers = peers + tmp_peers
     except:
         pass
     return peers
 
-peers = getPeers('nodes.wavesnodes.com') + getPeers('127.0.0.1:6869')
+peers = getPeers('https://nodes.wavesnodes.com') + getPeers('http://127.0.0.1:6869')
 nodes_by_country = {}
 
 print("Getting IP networks info...")
@@ -42,30 +49,33 @@ for peer in peers:
     if match:
         ip = match.group(1)
         if ip not in zip(*nodes)[1]:
-            res = IPWhois(ip).lookup_whois()['nets'][0]
-            country = ''
-            net = ''
-            descr = ''
             try:
-                if res['country']:
-                    country = res['country']
-                if res['name']:
-                    net = res['name']
-                if res['description']:
-                    descr = res['description'].replace('\n', ' ')
-            except:
-                pass
-            name = peer['peerName'][:30]
-            try:
-                if name[0]=='<':
+                res = IPWhois(ip).lookup_whois()['nets'][0]
+                country = ''
+                net = ''
+                descr = ''
+                try:
+                    if res['country']:
+                        country = res['country']
+                    if res['name']:
+                        net = res['name']
+                    if res['description']:
+                        descr = res['description'].replace('\n', ' ')
+                except:
+                    pass
+                try:
+                    name = peer['peerName'][:30]
+                    if name[0]=='<':
+                        name = ''
+                except:
                     name = ''
+                nodes.append((name, ip, country[:2], net[:20], descr[:40]))
+                if country in nodes_by_country.keys():
+                    nodes_by_country[country] += 1
+                else:
+                    nodes_by_country[country] = 1
             except:
                 pass
-            nodes.append((name, ip, country[:2], net[:20], descr[:40]))
-            if country in nodes_by_country.keys():
-                nodes_by_country[country] += 1
-            else:
-                nodes_by_country[country] = 1
                 
 nodes = nodes[1:]
 print
@@ -83,5 +93,7 @@ print("Country  # of nodes")
 print("-" * 20)
 for c in sorted(nodes_by_country.items(), key=lambda x: -x[1]):
     print ("  %-2s        %3d" % (c[0], c[1]))
+print("-" * 20)
+print ("            %3d" % sum(nodes_by_country.values()))
 print
 print("-" * 120)
