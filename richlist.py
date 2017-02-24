127e982c8e0460486e91c7d4534cb95a83f15436
PyWaves <dev@pywaves.org>
2017-02-24 10:20:36 +0100

    Add files via upload

--- richlist.py
+++ richlist.py
@@ -1,33 +1,34 @@
 import requests
-import math
-node = '127.0.0.1'
+import argparse
+
+NODE = 'http://127.0.0.1:6869'
+
+parser = argparse.ArgumentParser(description='Waves Rich List')
+parser.add_argument('-t', '--top', type=int, help='lists only the specified top positions')
+
+args = parser.parse_args()
+if args.top:
+    top = args.top
+else:
+    top = -1
+
+states = requests.get('%s/debug/state' % NODE).json()
+print("-" * 64)
+print(str.center("Waves Rich List", 64))
+if top > 0:
+    print(str.center("Top %d balances" % top, 64))
+print("  #    Address                                      Balance")
+print("-" * 64)
+n = 0
 total_balance = 0
-addresses = []
-unique_addresses = []
-top = 50    # 0 for all
-last = requests.get('http://' + node + ':6869/blocks/height').json()['height']
-for n in range(0, int(math.ceil(last / 100.0))):
-    for block in requests.get('http://%s:6869/blocks/seq/%d/%d' % (node, 1 + n * 100, min(last, (n + 1) * 100))).json():
-        txs = block['transactions']
-        for tx in txs:
-            try:
-                addresses.append(tx['recipient'])
-            except:
-                continue
-for address in set(addresses):
-    address_balance = float(requests.get('http://' + node + ':6869/addresses/balance/' + address).json()['balance']) / 100000000
-    if address_balance>0:
-        unique_addresses.append((address, address_balance))
+for i in sorted(states.items(), key=lambda x: -x[1]):
+    balance = i[1]
+    if len(i[0]) == 35 and balance > 0:
+        address = i[0]
+        total_balance += balance
+        n += 1
+        print("%6d %-38s %18.8f " % (n, address, balance / 1e8))
+        if n == top:
+            break
 print("-" * 64)
-print(str.center(("Waves Rich List"), 64))
-if(top>0):
-    print(str.center(("Top %d" % top), 64))
-print
-print("  #    Address                                        Balance")
-print("-" * 64)
-for i, address in enumerate(sorted(unique_addresses, key=lambda x: -x[1])[:top if top>0 else len(unique_addresses)]):
-    total_balance += address[1]
-    print("%6d %-38s %18.8f " % (i + 1, address[0], address[1]))
-print("-" * 64)
-print("                                              %18.8f" % (total_balance))
-     
+print("                                              %18.8f" % (total_balance / 1e8))
